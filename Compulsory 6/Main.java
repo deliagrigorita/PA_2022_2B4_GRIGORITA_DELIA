ae134fe078d2a188c9d876a6abec6f4f980fd6ec
deliagrigorita <deliagrigorita@yahoo.com>
2022-04-02 21:58:20 +0300

    Compulsory 6

---
+++ Compulsory 6/Main.java
@@ -0,0 +1,13 @@
+import com.gui.GuiDrawer;
+import com.gui.MainFrame;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        GuiDrawer guiDrawer = new GuiDrawer();
+        guiDrawer.runInterface();
+
+        //MainFrame mainFrame = new MainFrame();
+
+    }
+}
